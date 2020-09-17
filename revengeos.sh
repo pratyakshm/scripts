e25d3b2a93418d91048db379d7a08ebdffee1c74
pratyakshm <hello.pratyakshmehrotra@gmail.com>
2020-09-17 18:43:27 +0530

    revengeos.sh: changes

--- revengeos.sh
+++ revengeos.sh
@@ -1,8 +1,8 @@
 repo init -u https://github.com/RevengeOS/android_manifest -b r10.0
 repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
 git clone https://github.com/RevengeOS-Devices/device_xiaomi_rosy device/xiaomi/rosy
-git clone https://github.com/RevengeOS-Devices/kernel_xiaomi_rosy kernel/xiaomi/rosy
-git clone https://github.com/PratyakshM/vendor_xiaomi vendor/xiaomi
+git clone https://github.com/PratyakshM/kernel_xiaomi_rosy kernel/xiaomi/rosy
+git clone https://github.com/PratyakshM/vendor_xiaomi_rosy vendor/xiaomi/rosy
 source build/envsetup.sh
 lunch revengeos_rosy-user
 make bacon
