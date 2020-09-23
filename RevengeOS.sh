b921109c54e4256ef1f4ef8f287e0d23f327f4bf
pratyakshm <hello.pratyakshmehrotra@gmail.com>
2020-09-23 19:58:02 +0530

    RevengeOS 11

--- RevengeOS.sh
+++ RevengeOS.sh
@@ -1,8 +1,8 @@
-repo init -u https://github.com/RevengeOS/android_manifest -b r10.0
+repo init -u https://github.com/RevengeOS/android_manifest -b r11.0
 repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
-git clone https://github.com/RevengeOS-Devices/device_xiaomi_rosy device/xiaomi/rosy
-git clone https://github.com/PratyakshM/kernel_xiaomi_rosy kernel/xiaomi/rosy
-git clone https://github.com/PratyakshM/vendor_xiaomi_rosy vendor/xiaomi/rosy
+git clone https://github.com/RevengeOS-Devices/android_device_xiaomi_rosy device/xiaomi/rosy --branch r11.0 --single-branch --depth 1
+git clone https://github.com/PratyakshM/android_kernel_xiaomi_rosy kernel/xiaomi/rosy --depth 1
+git clone https://github.com/PratyakshM/android_vendor_xiaomi_rosy vendor/xiaomi/rosy --branch eleven --single-branch --depth 1 
 source build/envsetup.sh
 lunch revengeos_rosy-user
 make bacon
